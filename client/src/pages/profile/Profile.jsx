19183bd547e5f5db656ffbcf78e4ca622f3469ee
KyunGraphy <onhoan0@gmail.com>
2023-10-29 11:00:33 +0700

    FE | Display staff data on Profile page

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -7,53 +7,62 @@ import { AuthContext } from '../../contexts/AuthContext'
 import { Toastify } from '../../components/toastify/Toastify'
 import LeftPaper from './LeftPaper'
 import RightPaper from './RightPaper'
+import useFetch from '../../hooks/useFetch'
+import BackdropComponent from '../../components/backdrop/BackdropComponent'
 
 const Profile = () => {
   const { user, dispatch } = useContext(AuthContext)
+  const { loading, data: hotel } = useFetch(`/hotel/${user.hotelId}`)
 
   const [errMsg, setErrMsg] = useState(null);
 
   return (
     <React.Fragment>
-      {errMsg && <Toastify msg={errMsg} type="error" />}
-      <Navbar />
-      <Grid
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          flexWrap: 'wrap',
-          background: '#f2dcd0',
-          padding: '1em',
-        }}>
-        <Box
-          sx={{
-            display: 'flex',
-            height: '100%',
-            '& > :not(style)': {
-              margin: "1em",
-              minWidth: 560,
-              minHeight: 480,
-            },
-          }}
-        >
-          <LeftPaper user={user} setErrMsg={setErrMsg} dispatch={dispatch} />
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            '& > :not(style)': {
-              margin: "1em",
-              minWidth: 560,
-              minHeight: 225,
-            },
-          }}
-        >
-          <RightPaper user={user} />
-        </Box>
-      </Grid>
-      <Footer />
+      {loading ? (
+        <BackdropComponent />
+      ) : (
+        <React.Fragment>
+          {errMsg && <Toastify msg={errMsg} type="error" />}
+          <Navbar />
+          <Grid
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+              background: '#f2dcd0',
+              padding: '1em',
+            }}>
+            <Box
+              sx={{
+                display: 'flex',
+                height: '100%',
+                '& > :not(style)': {
+                  margin: "1em",
+                  minWidth: 560,
+                  minHeight: 480,
+                },
+              }}
+            >
+              <LeftPaper user={user} setErrMsg={setErrMsg} dispatch={dispatch} />
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                '& > :not(style)': {
+                  margin: "1em",
+                  minWidth: 560,
+                  minHeight: 225,
+                },
+              }}
+            >
+              <RightPaper user={user} hotel={hotel} />
+            </Box>
+          </Grid>
+          <Footer />
+        </React.Fragment>
+      )}
     </React.Fragment>
   )
 }
--- client/src/pages/profile/RightPaper.jsx
+++ client/src/pages/profile/RightPaper.jsx
@@ -3,26 +3,31 @@ import { Box, Typography } from '@mui/material';
 import { format } from "timeago.js"
 import { DemoPaper } from '../../constants/mui-components';
 
-const RightPaper = ({ user }) => {
+const RightPaper = ({ user, hotel }) => {
   return (
     <React.Fragment>
-      <DemoPaper elevation={3} variant="elevation" square={false}>
-        <h2><strong>Privacy</strong></h2>
-        <Box sx={{ textAlign: 'left', padding: '0 4em' }}>
-          <Typography sx={{ margin: '1em 0' }}><strong>Email:</strong> {user.email}</Typography>
-          <Typography sx={{ margin: '1em 0' }}><strong>Username:</strong> {user.username}</Typography>
-          <Typography sx={{ margin: '1em 0' }}><strong>Password:</strong> ******</Typography>
-          <Typography sx={{ margin: '1em 0' }}><strong>Created At:</strong> {format(user.createdAt)}</Typography>
-        </Box>
-      </DemoPaper>
-      <DemoPaper elevation={3} variant="elevation" square={false}>
-        <h2><strong>Staff</strong></h2>
-        <Box sx={{ textAlign: 'left', padding: '0 4em' }}>
-          <Typography sx={{ margin: '1em 0' }}><strong>Admin ID:</strong> MN001</Typography>
-          <Typography sx={{ margin: '1em 0' }}><strong>Role:</strong> Business Staff</Typography>
-          <Typography sx={{ margin: '1em 0' }}><strong>Salary:</strong> 2000$/month</Typography>
-        </Box>
-      </DemoPaper>
+      <React.Fragment>
+        <DemoPaper elevation={3} variant="elevation" square={false}>
+          <h2><strong>Privacy</strong></h2>
+          <Box sx={{ textAlign: 'left', padding: '0 4em' }}>
+            <Typography sx={{ margin: '1em 0' }}><strong>Email:</strong> {user.email}</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Username:</strong> {user.username}</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Password:</strong> ******</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Created At:</strong> {format(user.createdAt)}</Typography>
+          </Box>
+        </DemoPaper>
+        <DemoPaper elevation={3} variant="elevation" square={false}>
+          <h2><strong>Staff</strong></h2>
+          <Box sx={{ textAlign: 'left', padding: '0 4em' }}>
+            <Typography sx={{ margin: '1em 0' }}><strong>Admin ID: </strong>{user.adminId}</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Role: </strong>{user.role}</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Salary: </strong>{user.salary}$/month</Typography>
+            <Typography sx={{ margin: '1em 0' }}><strong>Department: </strong>
+              {hotel.department ? hotel.department : 'Hotel Manager'}
+            </Typography>
+          </Box>
+        </DemoPaper>
+      </React.Fragment>
     </React.Fragment>
   )
 }
